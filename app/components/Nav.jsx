160c7c0b7823714a71c442cdb7f8d689956cd86c
cokafo <cokafo@carlton.ca>
2018-04-23 10:25:41 -0400

    Updating styles on examples page, still can't get my nav to look right though

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -12,9 +12,15 @@ var Nav = React.createClass({
 				<div className="top-bar-left">
 					<ul className="menu">
 						<li className="menu-text">React Weather App</li>
-						<li><IndexLink to="/" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Get Weather</IndexLink></li>
-						<li><Link to="/about" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>About</Link></li>
-						<li><Link to="/examples" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Examples</Link></li>
+						<li>
+							<IndexLink to="/" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Get Weather</IndexLink>
+						</li>
+						<li>
+							<Link to="/about" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>About</Link>
+						</li>
+						<li>
+							<Link to="/examples" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Examples</Link>
+						</li>
 					</ul>
 				</div>
 				<div className="top-bar-right">
