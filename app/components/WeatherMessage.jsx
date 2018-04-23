3185862590213676be4f90eda9f3cc0378f4ee12
cokafo <cokafo@carlton.ca>
2018-04-23 11:09:47 -0400

    Styling get weather page

--- app/components/WeatherMessage.jsx
+++ app/components/WeatherMessage.jsx
@@ -3,7 +3,7 @@ var React = require('react');
 var WeatherMessage = ({temp, location}) => {
 	return (
 		<div>
-			<h4>It's {temp}°C in {location} right now</h4>
+			<h4 className="text-center">It's {temp}°C in {location} right now</h4>
 		</div>
 	)
 };
