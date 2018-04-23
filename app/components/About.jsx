36bd7b117f63a00aed1f90ace8b086be086bf779
cokafo <cokafo@carlton.ca>
2018-04-23 10:44:30 -0400

    Adding minimal styling to about page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -3,8 +3,11 @@ var React = require('react');
 var About = (props) => {
 	return (
 		<div>
-			<h3>About</h3>
-			<p>This is the about page, I think...</p>
+			<h1 className="text-center">About</h1>
+			<p>This is the about page, I think... Lorem ipsum dolor sit amet,
+			consectetur adipisicing elit. Eligendi praesentium iste consequatur non magnam
+			officia alias deleniti, repellendus soluta error obcaecati nulla a, sequi recusandae!
+			Quas aut, nobis dignissimos minima!</p>
 		</div>
 	)
 };
--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -5,6 +5,7 @@ var Main = (props) => {
 	return (
 		<div>
 			<Nav/>
+			<br />
 			<div className="row">
 				<div className="columns medium-6 large-4 small-centered">
 					{props.children}
